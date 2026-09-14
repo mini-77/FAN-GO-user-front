c76b43566706752ab627a43c7949dc580c61e9a9
mini-77 <ooooop7777@gmail.com>
2026-09-14 10:02:01 +0900

    Revert "챗봇 전송 버튼 아이콘을 레퍼런스처럼 재생(▶) 스타일 화살표로 교체"
    This reverts commit 0840492b3ad7b43e8c015f8a9a36fbbf86b30439.

--- src/ChatbotView.jsx
+++ src/ChatbotView.jsx
@@ -373,7 +373,7 @@ export default function ChatbotView() {
               disabled={isSending || !input.trim()}
               aria-label="전송"
             >
-              <Icon name="send" size={16} color="#fff" filled />
+              <Icon name="send" size={16} color="#fff" />
             </button>
           </div>
         </div>
